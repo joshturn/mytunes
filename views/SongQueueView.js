858a6d3b546cf9c7d4cf4db7144b45f43bd4f64e
joshturn <talktojosh@gmail.com>
2015-05-27 17:32:41 -0700

    Basic Player functionality and queue completed

---
+++ views/SongQueueView.js
@@ -0,0 +1,19 @@
+// SongQueueView.js - Defines a backbone view class for the song queue.
+var SongQueueView = Backbone.View.extend({
+
+  tagName: 'table',
+
+  initialize: function() {
+    this.render();
+    this.collection.on("add",this.render, this);
+    this.collection.on("remove",this.render, this);
+  },
+
+  render: function() {
+    this.$el.html('<th>Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+  }
+});
