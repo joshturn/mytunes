67af519e9d80a312598b8cddbe3ce6f38c6ac58d
joshturn <talktojosh@gmail.com>
2015-05-28 19:51:03 -0700

    Basic Trippy Visualizer

--- views/VisualView.js
+++ views/VisualView.js
@@ -1,6 +1,6 @@
 var VisualView = Backbone.View.extend({
 
-  el: '<canvas class="canvas" width="256" height="256"></canvas>',
+  el: '<canvas class="canvas" width="512" height="512"></canvas>',
 
   initialize: function() {
   },
@@ -8,9 +8,7 @@ var VisualView = Backbone.View.extend({
   display: function() {
     var audioctx = new window.AudioContext();
     var audioPlayer = document.getElementsByClassName("player");
-    console.log(audioPlayer);
     var audioSource = audioctx.createMediaElementSource($('audio')[0]);
-    console.log(audioSource)
 
     var analyser = audioctx.createAnalyser();
     audioSource.connect(analyser);
@@ -18,8 +16,8 @@ var VisualView = Backbone.View.extend({
 
     analyser.fftSize = 256;
     var analyserBuffer = analyser.frequencyBinCount;
-    audioData = new Uint8Array(analyserBuffer);
-    analyser.getByteFrequencyData(audioData);
+    var audioFreqData = new Uint8Array(analyserBuffer);
+    var audioTimeData = new Uint8Array(analyserBuffer);
 
     var canvas = $('canvas')[0];
     var canvasctx = canvas.getContext("2d");
@@ -29,33 +27,50 @@ var VisualView = Backbone.View.extend({
 
       drawFrame = requestAnimationFrame(draw);
 
-      analyser.getByteFrequencyData(audioData);
+      analyser.getByteFrequencyData(audioFreqData);
+      analyser.getByteTimeDomainData(audioTimeData);
 
+      // 'rgb(' + audioData[0] + ',' + audioData[50] + ',' + audioData[125] + ')';
       canvasctx.fillStyle = 'black';
       canvasctx.fillRect(0, 0, canvas.width, canvas.height);
-      canvasctx.lineWidth = 2;
-      canvas.stroke = 'white';
-      canvasctx.beginPath();
 
-      var sliceWidth = canvas.width * 1.0 / analyserBuffer;
-      var x = 0;
+      var rando = Math.floor(Math.random() * 10);
+      var topLeft = 0;
 
       for (var i = 0; i < analyserBuffer; i++) {
-        console.log(audioData[i]);
-        var v = audioData[i] / 128.0;
-        var y = v * canvas.height/2;
-
-        if(i === 0) {
-          canvasctx.moveTo(x, y);
-        } else {
-          canvasctx.lineTo(x, y);
+        // var gradient = canvasctx.createRadialGradient(topLeft, canvas.height - audioFreqData[i] *2, 10,topLeft + 4, canvas.height, 10);
+        var gradient = canvasctx.createLinearGradient(topLeft, canvas.height - audioFreqData[i] * 2,topLeft + 4, canvas.height);
+        gradient.addColorStop(0, 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')');
+        gradient.addColorStop(1, 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')');
+        if (i % 4 === 0) {
+          canvasctx.fillStyle = gradient;
+          // canvasctx.fillStyle = 'rgb(' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ',' + Math.floor(Math.random() * 256) + ')';
         }
-
-        x += sliceWidth;
+        canvasctx.fillRect(topLeft, canvas.height - audioFreqData[i] *2, topLeft + 4, canvas.height);
+        topLeft += 4;
       }
+      // canvasctx.lineWidth = 2;
+      // canvas.stroke = 'white';
+      // canvasctx.beginPath();
 
-      canvasctx.lineTo(canvas.width, canvas.height/2);
-      canvasctx.stroke();
+      // var sliceWidth = canvas.width * 1.0 / analyserBuffer;
+      // var x = 0;
+
+      // for (var i = 0; i < analyserBuffer; i++) {
+      //   var v = audioData[i] / 128.0;
+      //   var y = v * canvas.height/2;
+
+      //   if(i === 0) {
+      //     canvasctx.moveTo(x, y);
+      //   } else {
+      //     canvasctx.lineTo(x, y);
+      //   }
+
+      //   x += sliceWidth;
+      // }
+
+      // canvasctx.lineTo(canvas.width, canvas.height/2);
+      // canvasctx.stroke();
     }
     draw();
   },
